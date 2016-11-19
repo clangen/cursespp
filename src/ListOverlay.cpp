cc69170cb22f25a37a22aabffbe7d9e498918ada
Casey Langen <casey.langen@gmail.com>
2016-11-19 14:13:25 -0800

    Initial commit. Copied from musikbox so it can be useful for other apps as well.

---
+++ src/ListOverlay.cpp
@@ -0,0 +1,175 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2007-2016 casey langen
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the author nor the names of other contributors may
+//      be used to endorse or promote products derived from this software
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "ListOverlay.h"
+#include "Colors.h"
+#include "Screen.h"
+#include "Text.h"
+
+using namespace cursespp;
+
+#define VERTICAL_PADDING 2
+#define DEFAULT_WIDTH 22
+#define MAX_HEIGHT 12
+
+#define DISMISS() \
+    OverlayStack* overlays = this->GetOverlayStack(); \
+    if (overlays) { \
+        overlays->Remove(this); \
+    }
+
+ListOverlay::ListOverlay() {
+    this->SetFrameVisible(true);
+    this->SetFrameColor(CURSESPP_OVERLAY_FRAME);
+    this->SetContentColor(CURSESPP_OVERLAY_BACKGROUND);
+
+    this->width = this->height = 0;
+
+    this->listWindow.reset(new ListWindow());
+    this->listWindow->SetContentColor(CURSESPP_OVERLAY_BACKGROUND);
+    this->listWindow->SetFrameVisible(false);
+    this->listWindow->SetFocusOrder(0);
+    this->AddWindow(this->listWindow);
+}
+
+ListOverlay::~ListOverlay() {
+}
+
+void ListOverlay::Layout() {
+    this->RecalculateSize();
+
+    if (this->width > 0 && this->height > 0) {
+        this->MoveAndResize(
+            this->x,
+            this->y,
+            this->width,
+            this->height);
+
+        int listY = this->y + 3; /* below the border + title */
+        int listHeight = this->height - 4; /* top and bottom padding + title */
+
+        this->listWindow->MoveAndResize(
+            this->x + 2,
+            listY,
+            this->GetContentWidth() - 2,
+            listHeight);
+
+        this->Redraw();
+    }
+}
+
+ListOverlay& ListOverlay::SetTitle(const std::string& title) {
+    this->title = title;
+    this->RecalculateSize();
+    this->Layout();
+    this->Repaint();
+    return *this;
+}
+
+ListOverlay& ListOverlay::SetAdapter(IScrollAdapterPtr adapter) {
+    if (this->adapter != adapter) {
+        this->adapter = adapter;
+        this->listWindow->SetAdapter(adapter.get());
+    }
+
+    return *this;
+}
+
+ListOverlay& ListOverlay::SetItemSelectedCallback(ItemSelectedCallback cb) {
+    this->itemSelectedCallback = cb;
+    return *this;
+}
+
+bool ListOverlay::KeyPress(const std::string& key) {
+    if (key == "^[") { /* esc closes */
+        DISMISS();
+        return true;
+    }
+    else if (key == "KEY_ENTER") {
+        if (itemSelectedCallback) {
+            itemSelectedCallback(
+                this->adapter,
+                listWindow->GetSelectedIndex());
+        }
+        DISMISS();
+        return true;
+    }
+
+    return LayoutBase::KeyPress(key);
+}
+
+void ListOverlay::OnVisibilityChanged(bool visible) {
+    if (visible) {
+        this->SetFocus(this->listWindow);
+        this->Redraw();
+    }
+}
+
+void ListOverlay::RecalculateSize() {
+    this->width = DEFAULT_WIDTH;
+    this->height = 4; /* top frame + text + space + bottom frame */
+
+    if (this->adapter) {
+        this->height = std::min(
+            (int) MAX_HEIGHT,
+            (int) (4 + this->adapter->GetEntryCount()));
+    }
+
+    /* constrain to app bounds */
+    this->height = std::max(0, std::min(Screen::GetHeight() - 4, this->height));
+    this->width = std::max(0, std::min(Screen::GetWidth(), this->width));
+
+    this->y = VERTICAL_PADDING;
+    this->x = (Screen::GetWidth() / 2) - (this->width / 2);
+}
+
+void ListOverlay::Redraw() {
+    if (this->width <= 0 || this->height <= 0) {
+        return;
+    }
+
+    WINDOW* c = this->GetContent();
+
+    const int currentX = 1;
+    int currentY = 0;
+
+    if (this->title.size()) {
+        wmove(c, currentY, currentX);
+        wattron(c, A_BOLD);
+        wprintw(c, text::Ellipsize(this->title, this->width - 4).c_str());
+        wattroff(c, A_BOLD);
+        currentY += 2;
+    }
+}
---
+++ src/SimpleScrollAdapter.cpp
@@ -0,0 +1,104 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2007-2016 casey langen
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the author nor the names of other contributors may
+//      be used to endorse or promote products derived from this software
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+
+#include "SimpleScrollAdapter.h"
+#include "SingleLineEntry.h"
+#include "MultiLineEntry.h"
+#include "ScrollableWindow.h"
+#include "Colors.h"
+
+using namespace cursespp;
+
+#define MAX_ENTRY_COUNT 0xffffffff
+
+typedef IScrollAdapter::EntryPtr EntryPtr;
+
+SimpleScrollAdapter::SimpleScrollAdapter() {
+    this->maxEntries = MAX_ENTRY_COUNT;
+    this->selectable = false;
+}
+
+SimpleScrollAdapter::~SimpleScrollAdapter() {
+
+}
+
+void SimpleScrollAdapter::SetSelectable(bool selectable) {
+    this->selectable = selectable;
+}
+
+void SimpleScrollAdapter::Clear() {
+    this->entries.clear();
+}
+
+size_t SimpleScrollAdapter::GetEntryCount() {
+    return this->entries.size();
+}
+
+void SimpleScrollAdapter::SetMaxEntries(size_t maxEntries) {
+    this->maxEntries = maxEntries;
+}
+
+EntryPtr SimpleScrollAdapter::GetEntry(cursespp::ScrollableWindow* window, size_t index) {
+    auto entry = this->entries.at(index);
+
+    /* this is pretty damned gross, but super convenient. */
+    if (window && selectable) {
+        SingleLineEntry* single = dynamic_cast<SingleLineEntry*>(entry.get());
+        if (single) {
+            single->SetAttrs(-1LL);
+
+            if (index == window->GetScrollPosition().logicalIndex) {
+                single->SetAttrs(COLOR_PAIR(CURSESPP_HIGHLIGHTED_LIST_ITEM));
+            }
+        }
+    }
+
+    return entry;
+}
+
+void SimpleScrollAdapter::AddEntry(std::shared_ptr<IEntry> entry) {
+    entry->SetWidth(this->GetWidth());
+    entries.push_back(entry);
+
+    while (entries.size() > this->maxEntries) {
+        entries.pop_front();
+    }
+}
+
+void SimpleScrollAdapter::AddEntry(const std::string& value) {
+    EntryPtr entry(new SingleLineEntry(value));
+    this->AddEntry(entry);
+}
